c14d7c6b1e7c4c036505ccd9dfcf0022466ff3ee
Pan <zuboci@yandex.com>
2017-08-17 22:55:31 +0100

    Updated libssh2 batch script to use same MSVC compiler as Python uses

--- ci/appveyor/build_ssh2.bat
+++ ci/appveyor/build_ssh2.bat
@@ -1,12 +1,12 @@
 mkdir src && cd src
 
-%CMD_IN_ENV% cmake ..\libssh2 -G "NMake Makefiles"          ^
+cmake ..\libssh2 -G "NMake Makefiles"          ^
 	-DCMAKE_BUILD_TYPE=Release             ^
 	-DCRYPTO_BACKEND=WinCNG                ^
-	-G"Visual Studio 14"
+	-G"Visual Studio %MSVC_VER%"           ^
 	-DBUILD_SHARED_LIBS=OFF
 
-%CMD_IN_ENV% cmake --build . --config Release
+cmake --build . --config Release
 cd ..
 ls src/src
 cp src/src/libssh2.lib %PYTHON%/libs/ || cp src/src/Release/libssh2.lib %PYTHON%/libs/
