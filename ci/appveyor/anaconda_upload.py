f479fe37f4fd8639b7cdd31262f3f87bda401a66
Pan <zuboci@yandex.com>
2017-08-17 23:40:22 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Added dll bundling to wheel

---
+++ ci/appveyor/anaconda_upload.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from glob import glob
+import subprocess
+import traceback
+
+
+cmd = ['anaconda', '--login', 'parallelssh',
+       '--password', os.environ['ANACONDA_PASSWORD']]
+try:
+    subprocess.check_call(cmd)
+except subprocess.CalledProcessError:
+    traceback.print_exc()
+    sys.exit(1)
+cmd = ['anaconda', 'upload']
+cmd.extend(glob('*.tar.bz2'))
+try:
+    subprocess.check_call(cmd)
+except subprocess.CalledProcessError:
+    traceback.print_exc()
