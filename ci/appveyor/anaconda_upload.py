64747e99461e78d3da716ef5fdf373031d7ff18d
Pan <zuboci@yandex.com>
2017-08-17 23:51:17 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Added dll bundling to wheel

--- ci/appveyor/anaconda_upload.py
+++ ci/appveyor/anaconda_upload.py
@@ -5,12 +5,11 @@ import subprocess
 import traceback
 
 
-cmd = ['anaconda', '--login', 'parallelssh',
+cmd = ['anaconda', 'login', '--username', 'parallelssh',
        '--password', os.environ['ANACONDA_PASSWORD']]
 try:
     subprocess.check_call(cmd)
-except subprocess.CalledProcessError:
-    traceback.print_exc()
+except subprocess.CalledProcessError, ex:
     sys.exit(1)
 cmd = ['anaconda', 'upload']
 cmd.extend(glob('*.tar.bz2'))
