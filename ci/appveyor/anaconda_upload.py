7742f24fe4a9fb906a7cb630ec133ad646bd57f9
Pan <zuboci@yandex.com>
2017-08-18 01:48:08 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! Moved conda-recipe source to git url

--- ci/appveyor/anaconda_upload.py
+++ ci/appveyor/anaconda_upload.py
@@ -5,14 +5,7 @@ import subprocess
 import traceback
 
 
-subprocess.check_call(['anaconda', 'logout'])
-cmd = ['anaconda', 'login', '--username', 'parallelssh',
-       '--password', os.environ['ANACONDA_PASSWORD']]
-try:
-    subprocess.check_call(cmd)
-except subprocess.CalledProcessError, ex:
-    sys.exit(1)
-cmd = ['anaconda', 'upload']
+cmd = ['anaconda', '-t', os.environ['ANACONDA_TOKEN'], 'upload']
 cmd.extend(glob('*.tar.bz2'))
 try:
     subprocess.check_call(cmd)
