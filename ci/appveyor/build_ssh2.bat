d618497057aceeeffbfab538a976b6fbc5350108
Pan <zuboci@yandex.com>
2017-08-18 16:21:34 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Added binary wheel building on appveyor

--- ci/appveyor/build_ssh2.bat
+++ ci/appveyor/build_ssh2.bat
@@ -3,7 +3,7 @@ mkdir src && cd src
 %CMD_IN_ENV% cmake ..\libssh2 -G "NMake Makefiles"          ^
 	-DCMAKE_BUILD_TYPE=Release             ^
 	-DCRYPTO_BACKEND=WinCNG                ^
-	-DCMAKE_C_COMPILER="C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\BIN" ^
+	-DCMAKE_C_COMPILER="C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\BIN\\cl.exe" ^
 	-DBUILD_SHARED_LIBS=OFF
 
 %CMD_IN_ENV% cmake --build . --config Release
