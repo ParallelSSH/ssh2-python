e354b19a78f8f48134b8a07e9b5cb7fd065605c5
Pan <zuboci@yandex.com>
2017-08-18 13:18:02 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Added binary wheel building on appveyor

--- ci/appveyor/build_ssh2.bat
+++ ci/appveyor/build_ssh2.bat
@@ -5,6 +5,7 @@ cmake ..\libssh2 -G "NMake Makefiles"          ^
 	-DCRYPTO_BACKEND=WinCNG                ^
 	-DBUILD_SHARED_LIBS=OFF
 
-cmake --build . --config Release --target INSTALL
+cmake --build . --config Release
 cd ..
 ls src/src
+cp src/src/libssh2.lib C:/Python27-x64/libs/
