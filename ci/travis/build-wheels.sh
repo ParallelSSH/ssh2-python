bea17aacd7e0d16af3b8a0bcdd1a64cdf2d558dd
Pan <zuboci@yandex.com>
2017-12-27 12:49:26 +0000

    Updated manylinux wheel builds     Updated osx wheel build

--- ci/travis/build-wheels.sh
+++ ci/travis/build-wheels.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -xe
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    # "${PYBIN}/pip" install -r /io/dev-requirements.txt
+for PYBIN in `ls -1d /opt/python/*/bin | grep -v cpython`; do
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
@@ -12,7 +11,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in `ls -1d /opt/python/*/bin | grep -v cpython`; do
     "${PYBIN}/pip" install ssh2-python --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/python" -c 'from ssh2.session import Session; Session()')
 done
