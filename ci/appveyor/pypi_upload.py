be09668640b3f2988950a3c7100261dd2ecfec1a
Pan <zuboci@yandex.com>
2018-03-17 21:54:12 +0100

    Appveyor fixes

--- ci/appveyor/pypi_upload.py
+++ ci/appveyor/pypi_upload.py
@@ -1,11 +1,13 @@
-from __future__ import print_function
-
 import sys
 import subprocess
 import os
 
 
 def upload_pypi(files):
+    repo_tag = os.environ['APPVEYOR_REPO_TAG']
+    if repo_tag == 'false':
+        sys.stderr.write("Not a tagged release, skipping upload" + os.linesep)
+        return
     _user, _pass = os.environ['PYPI_USER'], os.environ['PYPI_PASS']
     try:
         subprocess.check_call(['twine', 'upload', '-u', _user,
