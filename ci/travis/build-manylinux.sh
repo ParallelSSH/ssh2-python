2298fe30bc8771876f38a7cd6dc13623dd27b5be
Pan <zuboci@yandex.com>
2019-06-23 13:35:40 +0100

    Fix linux wheel build versioning information.

--- ci/travis/build-manylinux.sh
+++ ci/travis/build-manylinux.sh
@@ -3,6 +3,7 @@
 docker_tag="parallelssh/ssh2-manylinux"
 
 rm -rf build dist ssh2/libssh2.*
+python ci/appveyor/fix_version.py .
 
 docker pull $docker_tag || echo
 docker build --cache-from $docker_tag ci/docker/manylinux -t $docker_tag
