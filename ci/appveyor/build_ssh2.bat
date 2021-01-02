fa83054d3b65c3cbf3bab96da10523782d282207
Panos <zuboci@yandex.com>
2021-01-02 19:18:33 +0000

    Updated ci script

--- ci/appveyor/build_ssh2.bat
+++ ci/appveyor/build_ssh2.bat
@@ -4,7 +4,6 @@ cd src
 cmake ..\libssh2 -G "NMake Makefiles"         ^
        -DCMAKE_BUILD_TYPE=Release             ^
        -DCRYPTO_BACKEND=OpenSSL               ^
-       -A x64                                 ^
        -DBUILD_SHARED_LIBS=OFF                ^
        -DENABLE_ZLIB_COMPRESSION=ON           ^
        -DENABLE_CRYPT_NONE=ON                 ^
