90d145eeca24615b27ae8a68931b7134771bce42
Panos <danst@tutanota.com>
2025-01-12 19:23:28 +0000

    Updated appveyor cfg

---
+++ ci/appveyor/build_package.bat
@@ -0,0 +1,17 @@
+IF NOT "%APPVEYOR_REPO_TAG%" == "true" (exit 0)
+
+dir ssh2/
+
+for %%I in (%PYTHONVERS%) do %%I\python.exe -V
+for %%I in (%PYTHONVERS%) do %%I\python.exe setup.py build_ext
+for %%I in (%PYTHONVERS%) do %%I\python.exe setup.py build
+for %%I in (%PYTHONVERS%) do %%I\python.exe setup.py install
+
+dir ssh2/
+
+cd dist
+for %%I in (%PYTHONVERS%) do %%I\python.exe -c "from ssh2.session import Session; Session()"
+cd ..
+
+for %%I in (%PYTHONVERS%) do %%I\python.exe setup.py bdist_wheel
+mv dist/* .
